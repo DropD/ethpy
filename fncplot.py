b73214074b02261b8c65e6e8e885a1630dba1a6a
Rico Häuselmann <r.haeuselmann@gmx.ch>
2013-03-08 09:50:22 +0100

    add custom title function to suit y label positioning.

--- fncplot.py
+++ fncplot.py
@@ -2,10 +2,12 @@ import matplotlib.pyplot as plt
 
 class fncplot(object):
     '''
-    plotting utilities for generating FNC style plots.
-     - xlabel: (static) alias to pyplot.xlabel
-     - ylabel: (static) places the y label on top of the y axis 
+    (static class) plotting utilities for generating FNC style plots.
+     - xlabel: alias to pyplot.xlabel
+     - ylabel: places the y label on top of the y axis 
                in horizontal rotation using pyplot.text
+     - title:  same as pyplot.title but shiftet up due to custom
+               ylabel position
     '''
     @staticmethod
     def xlabel(*args, **kwargs):
@@ -15,4 +17,14 @@ class fncplot(object):
     def ylabel(*args, **kwargs):
         subplot = plt.gcf().get_axes()
         kwargs['transform'] = subplot[0].transAxes
-        plt.text(0, 1.04, *args, **kwargs)
+        x = 0
+        y = 1.04
+        if 'offset' in kwargs:
+            x += offset[0]
+            y += offset[1]
+        plt.text(x, y, *args, **kwargs)
+
+    @staticmethod
+    def title(*args, **kwargs):
+        kwargs['y'] = 1.06
+        plt.title(*args, **kwargs)
